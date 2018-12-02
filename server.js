04418a63322ca147e87562789dff030f7d11cb72
AoifeKettle <akettle@tcd.ie>
2018-12-02 23:54:08 +0000

    clean version

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const hostname = '127.0.0.1';
 const port = 3000;
 //var server = require('./app');
 
-
 fs = require('fs');
 
 
@@ -13,9 +12,7 @@ fs.readFile('./graph/d3.html', function (err, html) {
         throw err;
     }
     const server = http.createServer((req, res) => {
-      //res.statusCode = 200;
-//       Content-Type: text/html; charset=utf-8
-// Content-Type: multipart/form-data; boundary=something
+
       res.setHeader('Content-Type', 'text/html');
       res.write(html);
       res.end();
